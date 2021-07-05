ad284352df00cb0348e6179534d0292224cf54c2
keitaro miyano <miyakeionigiri@yahoo.co.jp>
2021-07-05 11:13:07 +0900

    TypeScriptでリファクタリング

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -3,8 +3,17 @@ import { getAllPostIds, getPostData } from '../../lib/posts'
 import Head from 'next/head'
 import Date from '../../components/date'
 import utilStyles from '../../styles/utils.module.css'
+import { GetStaticProps, GetStaticPaths } from 'next'
 
-export default function Post({ postData }) {
+export default function Post({
+  postData
+}: {
+  postData: {
+    title: string
+    date: string
+    contentHtml: string
+  }
+}) {
   return (
     <Layout>
       <Head>
@@ -21,7 +30,7 @@ export default function Post({ postData }) {
   )
 }
 
-export async function getStaticPaths() {
+export const getStaticPaths: GetStaticPaths = async () => {
   // idとしてとりうる値のリストを返す
   const paths = getAllPostIds()
   return {
@@ -30,9 +39,9 @@ export async function getStaticPaths() {
   }
 }
 
-export async function getStaticProps({ params }) {
+export const getStaticProps: GetStaticProps = async ({ params }) => {
   // params.idを使用して、ブログの投稿に必要なデータを取得する。
-  const postData = await getPostData(params.id)
+  const postData = await getPostData(params.id as string)
   return {
     props: {
       postData
